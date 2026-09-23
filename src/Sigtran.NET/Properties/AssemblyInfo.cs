6824e21660972884dd331034f960adf55220200b
Ammar Heidari <ammar@arad-itc.org>
2026-09-23 11:40:18 +0330

    test: expose dispatch internals to regression harness

--- src/Sigtran.NET/Properties/AssemblyInfo.cs
+++ src/Sigtran.NET/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Sigtran.NET.Tests")]
 [assembly: InternalsVisibleTo("Sigtran.NET.HaTests")]
+[assembly: InternalsVisibleTo("Sigtran.NET.HaDispatchTests")]
