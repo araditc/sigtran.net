55cd59bda04b16a6e902c3e036e7a56b71ce3d80
Ammar Heidari <ammar@arad-itc.org>
2026-09-23 14:28:40 +0330

    test: expose HA internals to runtime regression suite

--- src/Sigtran.NET/Properties/AssemblyInfo.cs
+++ src/Sigtran.NET/Properties/AssemblyInfo.cs
@@ -3,3 +3,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Sigtran.NET.Tests")]
 [assembly: InternalsVisibleTo("Sigtran.NET.HaTests")]
 [assembly: InternalsVisibleTo("Sigtran.NET.HaDispatchTests")]
+[assembly: InternalsVisibleTo("Sigtran.NET.HaCoordinatorTests")]
+[assembly: InternalsVisibleTo("Sigtran.NET.HaRuntimeTests")]
